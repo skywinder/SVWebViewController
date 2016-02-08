d7b500198a1ef2ba319264e2622c1ed47eeb992d
Petr Korolev <sky4winder@gmail.com>
2016-02-08 17:35:52 +0200

    pretify

--- SVWebViewController/SVWebViewController.m
+++ SVWebViewController/SVWebViewController.m
@@ -12,14 +12,14 @@
 
 @interface SVWebViewController () <UIWebViewDelegate>
 
-@property (nonatomic, strong) UIBarButtonItem *backBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *forwardBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *refreshBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *stopBarButtonItem;
-@property (nonatomic, strong) UIBarButtonItem *actionBarButtonItem;
+@property(nonatomic, strong) UIBarButtonItem *backBarButtonItem;
+@property(nonatomic, strong) UIBarButtonItem *forwardBarButtonItem;
+@property(nonatomic, strong) UIBarButtonItem *refreshBarButtonItem;
+@property(nonatomic, strong) UIBarButtonItem *stopBarButtonItem;
+@property(nonatomic, strong) UIBarButtonItem *actionBarButtonItem;
 
-@property (nonatomic, strong) UIWebView *webView;
-@property (nonatomic, strong) NSURLRequest *request;
+@property(nonatomic, strong) UIWebView *webView;
+@property(nonatomic, strong) NSURLRequest *request;
 
 @end
 
@@ -39,11 +39,11 @@
     return [self initWithURL:[NSURL URLWithString:urlString]];
 }
 
-- (instancetype)initWithURL:(NSURL*)pageURL {
+- (instancetype)initWithURL:(NSURL *)pageURL {
     return [self initWithURLRequest:[NSURLRequest requestWithURL:pageURL]];
 }
 
-- (instancetype)initWithURLRequest:(NSURLRequest*)request {
+- (instancetype)initWithURLRequest:(NSURLRequest *)request {
     self = [super init];
     if (self) {
         self.request = request;
@@ -51,7 +51,7 @@
     return self;
 }
 
-- (void)loadRequest:(NSURLRequest*)request {
+- (void)loadRequest:(NSURLRequest *)request {
     [self.webView loadRequest:request];
 }
 
@@ -79,9 +79,9 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     NSAssert(self.navigationController, @"SVWebViewController needs to be contained in a UINavigationController. If you are presenting SVWebViewController modally, use SVModalWebViewController instead.");
-    
+
     [super viewWillAppear:animated];
-    
+
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
         [self.navigationController setToolbarHidden:NO animated:animated];
     }
@@ -92,7 +92,7 @@
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
-    
+
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
         [self.navigationController setToolbarHidden:YES animated:animated];
     }
@@ -106,14 +106,14 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
         return YES;
-    
+
     return toInterfaceOrientation != UIInterfaceOrientationPortraitUpsideDown;
 }
 
 #pragma mark - Getters
 
-- (UIWebView*)webView {
-    if(!_webView) {
+- (UIWebView *)webView {
+    if (!_webView) {
         _webView = [[UIWebView alloc] initWithFrame:[UIScreen mainScreen].bounds];
         _webView.delegate = self;
         _webView.scalesPageToFit = YES;
@@ -169,47 +169,43 @@
 - (void)updateToolbarItems {
     self.backBarButtonItem.enabled = self.self.webView.canGoBack;
     self.forwardBarButtonItem.enabled = self.self.webView.canGoForward;
-    
+
     UIBarButtonItem *refreshStopBarButtonItem = self.self.webView.isLoading ? self.stopBarButtonItem : self.refreshBarButtonItem;
-    
+
     UIBarButtonItem *fixedSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
     UIBarButtonItem *flexibleSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-    
+
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
         CGFloat toolbarWidth = 250.0f;
         fixedSpace.width = 35.0f;
-        
-        NSArray *items = [NSArray arrayWithObjects:
-                          fixedSpace,
-                          refreshStopBarButtonItem,
-                          fixedSpace,
-                          self.backBarButtonItem,
-                          fixedSpace,
-                          self.forwardBarButtonItem,
-                          fixedSpace,
-                          self.actionBarButtonItem,
-                          nil];
-        
+
+        NSArray *items = @[fixedSpace,
+                refreshStopBarButtonItem,
+                fixedSpace,
+                self.backBarButtonItem,
+                fixedSpace,
+                self.forwardBarButtonItem,
+                fixedSpace,
+                self.actionBarButtonItem];
+
         UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0.0f, 0.0f, toolbarWidth, 44.0f)];
         toolbar.items = items;
         toolbar.barStyle = self.navigationController.navigationBar.barStyle;
         toolbar.tintColor = self.navigationController.navigationBar.tintColor;
         self.navigationItem.rightBarButtonItems = items.reverseObjectEnumerator.allObjects;
     }
-    
+
     else {
-        NSArray *items = [NSArray arrayWithObjects:
-                          fixedSpace,
-                          self.backBarButtonItem,
-                          flexibleSpace,
-                          self.forwardBarButtonItem,
-                          flexibleSpace,
-                          refreshStopBarButtonItem,
-                          flexibleSpace,
-                          self.actionBarButtonItem,
-                          fixedSpace,
-                          nil];
-        
+        NSArray *items = @[fixedSpace,
+                self.backBarButtonItem,
+                flexibleSpace,
+                self.forwardBarButtonItem,
+                flexibleSpace,
+                refreshStopBarButtonItem,
+                flexibleSpace,
+                self.actionBarButtonItem,
+                fixedSpace];
+
         self.navigationController.toolbar.barStyle = self.navigationController.navigationBar.barStyle;
         self.navigationController.toolbar.tintColor = self.navigationController.navigationBar.tintColor;
         self.toolbarItems = items;
@@ -221,7 +217,7 @@
 - (void)webViewDidStartLoad:(UIWebView *)webView {
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
     [self updateToolbarItems];
-    
+
     if ([self.delegate respondsToSelector:@selector(webViewDidStartLoad:)]) {
         [self.delegate webViewDidStartLoad:webView];
     }
@@ -230,13 +226,13 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-    
+
     if (self.navigationItem.title == nil) {
         self.navigationItem.title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
     }
-    
+
     [self updateToolbarItems];
-    
+
     if ([self.delegate respondsToSelector:@selector(webViewDidFinishLoad:)]) {
         [self.delegate webViewDidFinishLoad:webView];
     }
@@ -245,7 +241,7 @@
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
     [self updateToolbarItems];
-    
+
     if ([self.delegate respondsToSelector:@selector(webView:didFailLoadWithError:)]) {
         [self.delegate webView:webView didFailLoadWithError:error];
     }
@@ -255,7 +251,7 @@
     if ([self.delegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)]) {
         return [self.delegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
     }
-    
+
     return YES;
 }
 
@@ -282,23 +278,22 @@
     NSURL *url = self.webView.request.URL ? self.webView.request.URL : self.request.URL;
     if (url != nil) {
         NSArray *activities = @[[SVWebViewControllerActivitySafari new], [SVWebViewControllerActivityChrome new]];
-        
+
         if ([[url absoluteString] hasPrefix:@"file:///"]) {
             UIDocumentInteractionController *dc = [UIDocumentInteractionController interactionControllerWithURL:url];
             [dc presentOptionsMenuFromRect:self.view.bounds inView:self.view animated:YES];
         } else {
             UIActivityViewController *activityController = [[UIActivityViewController alloc] initWithActivityItems:@[url] applicationActivities:activities];
-            
+
 #ifdef __IPHONE_8_0
             if (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_7_1 &&
-                UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-            {
+                    UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
                 UIPopoverPresentationController *ctrl = activityController.popoverPresentationController;
                 ctrl.sourceView = self.view;
                 ctrl.barButtonItem = sender;
             }
 #endif
-            
+
             [self presentViewController:activityController animated:YES completion:nil];
         }
     }
